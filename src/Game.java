bcb03184d49f8365b335e6d39f3ebe753e0c3a69
markov94ggiz <simeon.markov1994@gmail.com>
2015-05-27 00:08:56 +0300

    Progressing...

--- src/Game.java
+++ src/Game.java
@@ -6,6 +6,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Game extends JFrame {
@@ -40,50 +42,91 @@ public class Game extends JFrame {
     public Game() {
         setTitle("Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 575, 300);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
-        contentPane.setLayout(null);
         
         JLabel labelA = new JLabel("");
         labelA.setBounds(10, 203, 16, 23);
+        labelA.setVisible(false);
+        contentPane.setLayout(null);
         labelA.setIcon(new ImageIcon("C:\\Users\\Simo\\Desktop\\pics\\kk.png"));
         contentPane.add(labelA);
         
         JLabel labelC = new JLabel("");
         labelC.setBounds(10, 237, 16, 14);
+        labelC.setVisible(false);
         labelC.setIcon(new ImageIcon("C:\\Users\\Simo\\Desktop\\pics\\cross.png"));
         contentPane.add(labelC);
         
         JLabel labelB = new JLabel("");
-        labelB.setBounds(252, 212, 16, 14);
+        labelB.setBounds(390, 212, 16, 14);
+        labelB.setVisible(false);
         labelB.setIcon(new ImageIcon("C:\\Users\\Simo\\Desktop\\pics\\cross.png"));
         contentPane.add(labelB);
         
         JLabel labelD = new JLabel("");
-        labelD.setBounds(252, 237, 16, 14);
+        labelD.setBounds(390, 237, 16, 14);
+        labelD.setVisible(false);
         labelD.setIcon(new ImageIcon("C:\\Users\\Simo\\Desktop\\pics\\cross.png"));
         contentPane.add(labelD);
         
+        JLabel labelScore = new JLabel("0");
+        labelScore.setBounds(388, 178, 46, 14);
+        contentPane.add(labelScore);
+        
         JRadioButton rdbtnNewRadioButton = new JRadioButton("A");
         rdbtnNewRadioButton.setBounds(32, 203, 109, 23);
+        rdbtnNewRadioButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(rdbtnNewRadioButton.isSelected() == true){
+                    labelA.setVisible(true);
+                    labelScore.setText(String.valueOf(Integer.parseInt(labelScore.getText()) + 300));
+                }
+            }
+        });
         contentPane.add(rdbtnNewRadioButton);
         
         JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("C");
         rdbtnNewRadioButton_1.setBounds(32, 232, 109, 23);
+        rdbtnNewRadioButton_1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(rdbtnNewRadioButton_1.isSelected() == true){
+                    labelA.setVisible(true);
+                    labelC.setVisible(true);
+                    
+                }
+            }
+        });
         contentPane.add(rdbtnNewRadioButton_1);
         
         JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("B");
-        rdbtnNewRadioButton_2.setBounds(274, 203, 109, 23);
+        rdbtnNewRadioButton_2.setBounds(412, 203, 141, 23);
+        rdbtnNewRadioButton_2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(rdbtnNewRadioButton_2.isSelected() == true){
+                    labelA.setVisible(true);
+                    labelB.setVisible(true);
+                }
+            }
+        });
         contentPane.add(rdbtnNewRadioButton_2);
         
         JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("D");
-        rdbtnNewRadioButton_3.setBounds(274, 232, 109, 23);
+        rdbtnNewRadioButton_3.setBounds(412, 232, 141, 23);
+        rdbtnNewRadioButton_3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(rdbtnNewRadioButton_3.isSelected() == true){
+                    labelA.setVisible(true);
+                    labelD.setVisible(true);
+                }
+            }
+        });
         contentPane.add(rdbtnNewRadioButton_3);
         
         JLabel labelLabirinth = new JLabel("");
-        labelLabirinth.setBounds(0, 11, 434, 156);
+        labelLabirinth.setBounds(0, 11, 434, 126);
         labelLabirinth.setIcon(new ImageIcon("C:\\Users\\Simo\\Desktop\\pics\\1282889440_46813_1.jpg"));
         contentPane.add(labelLabirinth);
         
@@ -91,16 +134,21 @@ public class Game extends JFrame {
         labelQuestion.setBounds(10, 178, 60, 14);
         contentPane.add(labelQuestion);
         
-        setLabelPlayer(new JLabel("LameGuest"));
-        getLabelPlayer().setBounds(252, 178, 60, 14);
-        contentPane.add(getLabelPlayer());
+        setPlayer(new JLabel("LameGuest"));
+        getPlayer().setBounds(252, 178, 60, 14);
+        contentPane.add(getPlayer());
+        
+        
+        
     }
+        
+        
 
-    public static JLabel getLabelPlayer() {
+    public static JLabel getPlayer() {
         return labelPlayer;
     }
 
-    public void setLabelPlayer(JLabel labelPlayer) {
+    public void setPlayer(JLabel labelPlayer) {
         Game.labelPlayer = labelPlayer;
     }
 
--- src/StartGame.java
+++ src/StartGame.java
@@ -6,12 +6,17 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 
 public class StartGame {
 
     private JFrame startGame;
     private JTextField textField;
+    private JLabel lblWriteYourNickname;
+    private JLabel lblNewLabel;
 
     /**
      * Launch the application.
@@ -47,30 +52,40 @@ public class StartGame {
     private void initialize() {
         startGame = new JFrame();
         startGame.setTitle("Start the game");
-        startGame.setBounds(100, 100, 450, 300);
+        startGame.setBounds(100, 100, 228, 237);
         startGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        startGame.getContentPane().setLayout(null);
         
         JButton btnNewButton = new JButton("Play");
+        btnNewButton.setBounds(66, 56, 89, 23);
         btnNewButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 startGame.dispose();
                 Game game = new Game();
                 String toForm2 = text();
-                Game.getLabelPlayer().setText(toForm2);
+                Game.getPlayer().setText(toForm2);
                 game.setVisible(true);
                 
                 
             }
         });
-        btnNewButton.setBounds(108, 126, 89, 23);
+        startGame.getContentPane().setLayout(null);
         startGame.getContentPane().add(btnNewButton);
         btnNewButton.setFont(new Font("Tahoma",Font.BOLD,20));
         
         textField = new JTextField();
-        textField.setBounds(63, 30, 86, 20);
+        textField.setBounds(46, 31, 128, 20);
         startGame.getContentPane().add(textField);
         textField.setColumns(10);
+        
+        lblWriteYourNickname = new JLabel("Write your nickname here:");
+        lblWriteYourNickname.setForeground(new Color(102, 0, 0));
+        lblWriteYourNickname.setBounds(46, 11, 166, 14);
+        startGame.getContentPane().add(lblWriteYourNickname);
+        
+        lblNewLabel = new JLabel("");
+        lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Simo\\Desktop\\pics\\cat.jpg"));
+        lblNewLabel.setBounds(0, 0, 196, 192);
+        startGame.getContentPane().add(lblNewLabel);
         
         
       
