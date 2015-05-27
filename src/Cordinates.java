7a5c797d9c557a404160499878f9aaef880f2e54
lazarov1994 <lazarov686@gmail.com>
2015-05-28 00:12:45 +0300

    Class Cordinates?

--- src/Cordinates.java
+++ src/Cordinates.java
@@ -1,11 +1,39 @@
 /**
- * 
- */
-
-/**
  * @author Lazarov94
  *
  */
-public class Cordinates {
+class Cordinates {
+	int x;
+	int y;
 
+	Cordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void set(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	public int getY(){
+		return this.y;
+	}
+	
+	boolean equals(Cordinates a){
+		if(this.x == a.x && this.y== a.y){
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
