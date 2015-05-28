86233038019f05792d7905544237bd27821fc2d4
RadiDicheva <radchety@gmail.com>
2015-05-28 02:39:32 +0200

    Random questions

---
+++ src/RandomQuestion.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomQuestion {
+	private List<Question> easy = new ArrayList<Question>();
+	private List<Question> medium = new ArrayList<Question>();
+	private List<Question> hard = new ArrayList<Question>();
+	
+	public void filler (List<Question> listOfQuestions) {
+		
+		for (Question question : listOfQuestions) {
+			if(question.getId() == 1) {
+				easy.add(question);
+			} else if (question.getId() == 2) {
+				medium.add(question);
+			} else {
+				hard.add(question);
+			}
+		}
+	}
+	
+	public RandomQuestion () {
+		this.easy = filler(listOfQuestions);
+	}
+	
+	public Question getEasyQuestion (List<Question> easy) {
+		easy.addAll(this.easy);
+		Random rand = new Random();
+		int questionIndex = rand.nextInt(easy.size());
+		return easy.remove(questionIndex);
+	}
+	
+	public Question getMediumQuestion (List<Question> medium) {
+		medium.addAll(this.medium);
+		Random rand = new Random();
+		int questionIndex = rand.nextInt(medium.size());
+		return medium.remove(questionIndex);
+	}
+	
+	public Question getHardQuestion (List<Question> hard) {
+		hard.addAll(this.hard);
+		Random rand = new Random();
+		int questionIndex = rand.nextInt(hard.size());
+		return hard.remove(questionIndex);
+	}
+}
