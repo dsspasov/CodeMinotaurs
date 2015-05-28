dc58febab7ba0c80d1e62bc00c6bcf9f70a04cfa
dsspasov <dsspasov94@gmail.com>
2015-05-28 15:04:37 +0300

    JSONReader static get list of questions

--- src/JSONReader.java
+++ src/JSONReader.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+//import java.util.LinkedList;
 import java.util.List;
 
 import org.json.simple.JSONArray;
@@ -11,15 +12,10 @@ import org.json.simple.parser.JSONParser;
 public class JSONReader {
 
     private String path;
-    private List<Question> listOfQuestions;
-
+    public static List<Question> listOfQuestions;
+    
     public JSONReader(String path) {
         this.setPath(path);
-        this.listOfQuestions = new ArrayList<Question>();
-    }
-
-    public List<Question> getListOfQuestions() {
-        return listOfQuestions;
     }
 
     public String getPath() {
@@ -29,45 +25,53 @@ public class JSONReader {
     public void setPath(String path) {
         this.path = path;
     }
-
-    public void read() {
+    
+    public static List<Question> getListOfQuestions(){
+        return JSONReader.listOfQuestions;
+    }
+    
+    
+    
+    public List<Question> read() {
         JSONParser parser = new JSONParser();
+        JSONReader.listOfQuestions = null; 
         try {
 
             BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(this.getPath()), "UTF-8"));
-
+            JSONReader.listOfQuestions = new ArrayList<Question>();
+            
             Object obj = parser.parse(in);
-
+            
             JSONArray listOfObjects = (JSONArray) obj;
 
             for (Object object : listOfObjects) {
                 JSONObject jsonObject = (JSONObject) object;
-
                 String id = (String) jsonObject.get("id");
                 String question = (String) jsonObject.get("question");
                 String correctAnswer = (String) jsonObject.get("correctAnswer");
                 String category = (String) jsonObject.get("category");
                 JSONArray listOfAnswers = (JSONArray) jsonObject.get("listOfAnswers");
+                
                 List<String> answers = new ArrayList<String>();
+                
                 for (Object answer : listOfAnswers.toArray()) {
                     answers.add((String) answer);
                 }
-
-                this.listOfQuestions.add(new Question(question, answers, correctAnswer, category, id));
+                
+                JSONReader.listOfQuestions.add(new Question(question, answers, correctAnswer, category, id));
 
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return JSONReader.listOfQuestions;
 
     }
 /*
     public static void main(String[] args) {
         String path = "C:\\Users\\user\\Documents\\GitHub\\CodeMinotaurs\\questions.json";
         JSONReader x = new JSONReader(path);
-        x.read();
-        System.out.println(x.getListOfQuestions().get(0).getCorrectAnswer().toString());
-    }
-*/
+        System.out.println(x.read().get(0).getQuestion());
+    }*/
 }
